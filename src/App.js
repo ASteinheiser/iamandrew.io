1635efb47be19f1e566136e3e845eb3ff70a9fa9
Andrew Steinheiser <me@iamandrew.io>
2018-11-11 17:12:57 -0700

    remove unneeded useEffect and Events object from react-scroll

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Events, scroller }           from 'react-scroll';
-import VisibilitySensor               from 'react-visibility-sensor';
+import React, { useState } from 'react';
+import { scroller }        from 'react-scroll';
+import VisibilitySensor    from 'react-visibility-sensor';
 
 import TopBar from './components/TopBar';
 
 const App = (props) => {
 
   const [active, setActive] = useState('home');
-
-  useEffect(() => {
-    Events.scrollEvent.register('begin', () => {/*console.log("begin", arguments)*/});
-    Events.scrollEvent.register('end', () => {/*console.log("end", arguments)*/});
-    return () => {
-      Events.scrollEvent.remove('begin');
-      Events.scrollEvent.remove('end');
-    }
-  }, []); // call this effect only on mount
 
   function navigate(section) {
     scroller.scrollTo(section, {
