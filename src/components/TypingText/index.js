50ce406473adab6f6f5a8e93a217010694a080ae
Andrew Steinheiser <me@iamandrew.io>
2018-11-14 23:20:05 -0700

    make typing text work

--- src/components/TypingText/index.js
+++ src/components/TypingText/index.js
@@ -1,16 +1,35 @@
 import React from 'react';
 
+import TextType from './text-type';
+
 import './typing-text.scss';
 
+const TYPING_TEXT = [
+  'ANDREW',
+  'A FULLSTACK DEV',
+  'A CREATOR',
+  'A DESIGNER',
+];
+
 const TypingText = (props) => {
+
+  window.onload = function() {
+    var elements = document.getElementsByClassName('typewrite');
+
+    for (var i=0; i<elements.length; i++) {
+      new TextType(elements[i], TYPING_TEXT);
+    }
+  };
+
   return(
     <div className='typing-text-container'>
-      <span className='typing-text-main'>
-        {'I AM '}
-      </span>
-      <span className='typing-text-accent'>
-        {'ANDREW_'}
-      </span>
+
+      <div className='typewrite'>
+        <span className='typing-text-static'>I AM</span>
+        <span className='wrap'></span>
+        <span className='blink-cursor'>_</span>
+      </div>
+
     </div>
   );
 }
---
+++ src/components/TypingText/text-type.js
@@ -0,0 +1,48 @@
+const TYPE_SPEED = 2000;
+
+var TextType = function(el, toRotate) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = TYPE_SPEED;
+  this.text = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TextType.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullText = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.text = fullText.substring(0, this.text.length - 1);
+  } else {
+    this.text = fullText.substring(0, this.text.length + 1);
+  }
+
+  this.el.innerHTML = `
+    <span class='typing-text-static'>I AM</span>
+    <span class='wrap'>${this.text}</span>
+    <span class='blink-cursor'>_</span>
+  `;
+
+  var self = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.text === fullText) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.text === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function() {
+    self.tick();
+  }, delta);
+};
+
+export default TextType;
