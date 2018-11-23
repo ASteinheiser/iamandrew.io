ba3bca0c7b6fa9f46748b9d9823f5c4ea888dde2
Andrew Steinheiser <me@iamandrew.io>
2018-11-23 15:59:35 -0700

    finshed up footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,15 @@ import './footer.scss';
 const Footer = (props) => {
   return(
     <div className='footer-container'>
+
       {'Made with ♥ by Andrew Steinheiser'}
+
+      <a href='https://github.com/ASteinheiser/iamandrew.io'
+        target='_blank'
+        rel='noopener noreferrer'>
+        {'View Source Code'}
+      </a>
+
     </div>
   );
 }
