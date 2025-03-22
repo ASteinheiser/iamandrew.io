ea04b53a4e319013b6d49e438f239f43a3976293
ASteinheiser <me@iamandrew.io>
2025-03-22 01:23:38 -0700

    update sensor props

--- src/components/Sensor/index.tsx
+++ src/components/Sensor/index.tsx
@@ -3,6 +3,9 @@ import VisibilitySensor from 'react-visibility-sensor';
 interface SensorProps {
   children: React.ReactNode;
   onChange: (isVisible: boolean) => void;
+  minTopValue?: number;
+  partialVisible?: boolean;
+  partialVisibility?: 'top' | 'bottom' | 'both';
 }
 
 const Sensor = (props: SensorProps) => {
@@ -13,6 +16,7 @@ const Sensor = (props: SensorProps) => {
       minTopValue={300}
       partialVisible={true}
       partialVisibility={'top'}
+      {...props}
       onChange={onChange}
     >
       {children}
