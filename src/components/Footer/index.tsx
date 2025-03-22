9b3089e0c321e72fa6e16ed57ba83054eac94e7d
ASteinheiser <me@iamandrew.io>
2025-03-22 01:41:36 -0700

    add footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,19 @@
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      {'Made with ♥ by Andrew Steinheiser'}
+
+      <a
+        href="https://github.com/ASteinheiser/iamandrew.io"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {'View Source Code'}
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
