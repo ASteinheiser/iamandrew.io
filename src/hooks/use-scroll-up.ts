ad0cc4c6a0af2b1fdb97e36549bbf583787c9b0e
ASteinheiser <me@iamandrew.io>
2025-03-22 12:51:14 -0700

    refactor hooks

--- src/hooks/use-scroll-up.ts
+++ src/hooks/use-scroll-up.ts
@@ -1,24 +1,26 @@
 import { useState, useEffect } from 'react';
 
 const useScrollUp = () => {
-  const [lastScroll, setLastScroll] = useState(0);
+  const [_, setLastScroll] = useState(0);
   const [scrollUp, setScrollUp] = useState(false);
 
   useEffect(() => {
+    const onScroll = () => {
+      setLastScroll((currentScroll) => {
+        if (currentScroll > window.scrollY) {
+          setScrollUp(true);
+        } else {
+          setScrollUp(false);
+        }
+        return window.scrollY;
+      });
+    };
+
     window.addEventListener('scroll', onScroll);
     return () => {
       window.removeEventListener('scroll', onScroll);
     };
-  });
-
-  function onScroll() {
-    if (lastScroll > window.scrollY) {
-      setScrollUp(true);
-    } else {
-      setScrollUp(false);
-    }
-    setLastScroll(window.scrollY);
-  }
+  }, []);
 
   return scrollUp;
 };
--- src/hooks/use-window-size.ts
+++ src/hooks/use-window-size.ts
@@ -4,22 +4,19 @@ const useWindowSize = () => {
   const [height, setHeight] = useState(window.innerHeight);
   const [width, setWidth] = useState(window.innerWidth);
 
-  function updateWindowDimensions() {
-    setHeight(window.innerHeight);
-    setWidth(window.innerWidth);
-  }
-
   useEffect(() => {
+    const updateWindowDimensions = () => {
+      setHeight(window.innerHeight);
+      setWidth(window.innerWidth);
+    };
+
     window.addEventListener('resize', updateWindowDimensions);
     return () => {
       window.removeEventListener('resize', updateWindowDimensions);
     };
-  }, []); // we pass empty array as the second param to make this only call on mount and not on any updates
+  }, []);
 
-  return {
-    width,
-    height,
-  };
+  return { width, height };
 };
 
 export default useWindowSize;
