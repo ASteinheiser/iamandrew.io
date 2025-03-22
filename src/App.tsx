666b5b2889ca344870462370c3bdf3bf72b21f59
ASteinheiser <me@iamandrew.io>
2025-03-22 12:23:09 -0700

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -14,16 +14,16 @@ const App = () => {
   const [active, setActive] = useState('home');
   const scrollUp = useScrollUp();
 
-  function navigate(section: string) {
+  const navigate = (section: string) => {
     scroller.scrollTo(section, {
       duration: 800,
       delay: 0,
       smooth: 'easeInOutQuart',
       offset: -70,
     });
-  }
+  };
 
-  function changeVisible(isVisible: boolean, section: string) {
+  const changeVisible = (isVisible: boolean, section: string) => {
     if (isVisible) {
       setActive(section);
     }
@@ -31,7 +31,7 @@ const App = () => {
     // because the sensor component either detects elements
     // completely in view, or based off offset from top of elements.
     // this handles switching 'active' sections when scrolling up into
-    // sections that stretch past the screen height (specifically on Mobile)
+    // sections that stretch past the screen height (especially on Mobile)
     else if (!isVisible && scrollUp) {
       switch (section) {
         case 'work':
@@ -46,7 +46,7 @@ const App = () => {
         default:
       }
     }
-  }
+  };
 
   return (
     <>
