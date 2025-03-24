7d6fc5bfceb7df0e6fb6c60052bbdb6efd7b0630
ASteinheiser <me@iamandrew.io>
2025-03-23 21:58:42 -0700

    add enum for app sections

--- src/components/TopBar/index.tsx
+++ src/components/TopBar/index.tsx
@@ -1,39 +1,42 @@
 import Headroom from 'react-headroom';
-import { isMobile } from 'react-device-detect';
 
 import Logo from './logo.svg';
 import './top-bar.scss';
 
+export enum APP_SECTION {
+  STARS = 'stars',
+  WORK = 'work',
+  CONTACT = 'contact',
+}
+
 interface TopBarProps {
-  active: string;
-  navigate: (section: string) => void;
+  active: APP_SECTION;
+  navigate: (section: APP_SECTION) => void;
 }
 
 export const TopBar = ({ active, navigate }: TopBarProps) => {
   return (
     <Headroom>
-      <div className={`top-bar-container ${!isMobile ? 'top-bar-container--desktop' : ''}`}>
+      <div className="top-bar-container">
         <div className="max-width spacing">
-          <img className="top-bar-logo" src={Logo} alt="logo" onClick={() => navigate('home')} />
+          <img
+            className="top-bar-logo"
+            src={Logo}
+            alt="logo"
+            onClick={() => navigate(APP_SECTION.STARS)}
+          />
 
           <div className="top-bar-item-container">
             <div
-              className={'top-bar-item' + (active === 'work' ? ' active' : '')}
-              onClick={() => navigate('work')}
+              className={`top-bar-item ${active === APP_SECTION.WORK ? 'active' : ''}`}
+              onClick={() => navigate(APP_SECTION.WORK)}
             >
               {'WORK_'}
             </div>
 
             <div
-              className={'top-bar-item' + (active === 'journey' ? ' active' : '')}
-              onClick={() => navigate('journey')}
-            >
-              {'JOURNEY_'}
-            </div>
-
-            <div
-              className={'top-bar-item' + (active === 'contact' ? ' active' : '')}
-              onClick={() => navigate('contact')}
+              className={`top-bar-item ${active === APP_SECTION.CONTACT ? 'active' : ''}`}
+              onClick={() => navigate(APP_SECTION.CONTACT)}
             >
               {'CONTACT_'}
             </div>
