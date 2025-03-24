4f850c87f760107ab52fbce40358fb2b63309d04
ASteinheiser <me@iamandrew.io>
2025-03-24 16:35:15 -0700

    SASSy variables and style organization

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: './',
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "/src/styles/variables.scss" as *;',
+      },
+    },
+  },
 });
