12f2b84b9975782d2051551fb0e4419a32dd7fff
ASteinheiser <me@iamandrew.io>
2025-03-24 13:54:07 -0700

    basic layout of data for content section

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,5 +1,49 @@
+import ProfilePic from './assets/me.png';
+import Mail from './assets/mail.svg';
+import LinkedIn from './assets/linkedin.svg';
+import GitHub from './assets/github.svg';
+import Briefcase from './assets/briefcase.svg';
+
 import './contact.scss';
 
 export const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-container">
+      <img src={ProfilePic} alt="Andrew Steinheiser" className="contact-image" />
+
+      <div className="contact-content-divider" />
+
+      <div className="contact-content">
+        <div>
+          <img src={Mail} alt="email" />
+          <a href="mailto:me@iamandrew.io">me@iamandrew.io</a>
+        </div>
+
+        <div>
+          <img src={GitHub} alt="github" />
+          <a href="https://github.com/asteinheiser" target="_blank" rel="noopener noreferrer">
+            github/ASteinheiser
+          </a>
+        </div>
+
+        <div>
+          <img src={LinkedIn} alt="LinkedIn" />
+          <a
+            href="https://www.linkedin.com/in/asteinheiser/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            linkedin/ASteinheiser
+          </a>
+        </div>
+
+        <div>
+          <img src={Briefcase} alt="briefcase" />
+          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            View Resume
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
