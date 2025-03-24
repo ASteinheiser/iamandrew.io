a597ada8f5ba207588f29f51a753b54d604e9705
ASteinheiser <me@iamandrew.io>
2025-03-24 00:00:53 -0700

    rewrite how textType is attached to an element, using refs and effects

--- src/components/TypingText/index.tsx
+++ src/components/TypingText/index.tsx
@@ -1,22 +1,24 @@
+import { useEffect, useRef } from 'react';
+
 import { TextType } from './text-type';
 import './typing-text.scss';
 
 const TYPING_TEXT_WORDS = ['ANDREW', 'A DEVELOPER', 'A SKATER', 'A CREATOR'];
 
 export const TypingText = () => {
-  window.onload = function () {
-    const elements = document.getElementsByClassName('wrap');
+  const textRef = useRef<HTMLSpanElement>(null);
 
-    for (let i = 0; i < elements.length; i++) {
-      new TextType({ element: elements[i] as HTMLElement, wordRotation: TYPING_TEXT_WORDS });
+  useEffect(() => {
+    if (textRef.current) {
+      new TextType({ element: textRef.current, wordRotation: TYPING_TEXT_WORDS });
     }
-  };
+  }, []);
 
   return (
     <div className="typing-text-container">
       <div className="typewrite">
         <span className="typing-text-static">I AM </span>
-        <span className="wrap"></span>
+        <span ref={textRef} className="typing-text"></span>
         <span className="blink-cursor">_</span>
       </div>
     </div>
