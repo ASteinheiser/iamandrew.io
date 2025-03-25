c8f7843b7e1fa06259158eb2e5cdfa3cb304ca3c
ASteinheiser <me@iamandrew.io>
2025-03-25 16:26:05 -0700

    focus work items when in view on tablets and below

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -1,24 +1,36 @@
 import { useState } from 'react';
 import { InView } from 'react-intersection-observer';
 import { workList } from './work-list';
+import { useWindowSize } from '../../hooks/use-window-size';
 import './work.scss';
 
 export const Work = () => {
+  const { width } = useWindowSize();
+  const isTablet = width <= 1000;
+
   const [visibleId, setVisibleId] = useState<number | null>(null);
+
+  const handleChangeVisible = (index: number) => (isVisible: boolean) => {
+    if (isVisible) {
+      setVisibleId(index);
+    } else if (visibleId === index) {
+      setVisibleId(null);
+    }
+  };
 
   return (
     <div className="work-list-wrap">
       {workList.map(({ title, description, link, image }, index) => (
-        <InView
-          key={index}
-          threshold={1}
-          onChange={(isVisible) => setVisibleId(isVisible ? index : null)}
-        >
+        <InView as="div" key={index} threshold={1} onChange={handleChangeVisible(index)}>
           <a
             href={link}
             target="_blank"
             rel="noopener noreferrer"
-            className={`work-item ${index % 2 === 0 ? 'work-item-reverse' : ''}`}
+            className={`
+              work-item
+              ${index % 2 === 0 ? 'work-item-reverse' : ''}
+              ${isTablet && visibleId === index ? 'work-item-focused' : ''}
+            `}
           >
             <div className="work-item-content">
               <div className="work-item-title">{title}</div>
