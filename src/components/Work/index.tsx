bb6b741cd6af8d59a85f3d71b75339edc4e4fad7
ASteinheiser <me@iamandrew.io>
2025-03-25 16:09:45 -0700

    setup visibility sensor for work posts

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -1,25 +1,34 @@
+import { useState } from 'react';
+import { InView } from 'react-intersection-observer';
 import { workList } from './work-list';
 import './work.scss';
 
 export const Work = () => {
+  const [visibleId, setVisibleId] = useState<number | null>(null);
+
   return (
     <div className="work-list-wrap">
       {workList.map(({ title, description, link, image }, index) => (
-        <a
+        <InView
           key={index}
-          href={link}
-          target="_blank"
-          rel="noopener noreferrer"
-          className={`work-item ${index % 2 === 0 ? 'work-item-reverse' : ''}`}
+          threshold={1}
+          onChange={(isVisible) => setVisibleId(isVisible ? index : null)}
         >
-          <div className="work-item-content">
-            <div className="work-item-title">{title}</div>
-            <div className="work-item-description">{description}</div>
-          </div>
-          <div className="work-item-image">
-            <img src={image} alt={title} />
-          </div>
-        </a>
+          <a
+            href={link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={`work-item ${index % 2 === 0 ? 'work-item-reverse' : ''}`}
+          >
+            <div className="work-item-content">
+              <div className="work-item-title">{title}</div>
+              <div className="work-item-description">{description}</div>
+            </div>
+            <div className="work-item-image">
+              <img src={image} alt={title} />
+            </div>
+          </a>
+        </InView>
       ))}
     </div>
   );
