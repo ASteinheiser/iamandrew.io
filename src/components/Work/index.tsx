1d8c54954f7debf01bf3b7bdcd5f2b1171ba7a93
ASteinheiser <me@iamandrew.io>
2025-03-24 22:16:02 -0700

    add markup skeleton for blog posts

--- src/components/Work/index.tsx
+++ src/components/Work/index.tsx
@@ -4,8 +4,16 @@ import './work.scss';
 export const Work = () => {
   return (
     <div className="work-list-wrap">
-      {workList.map(({ title }) => (
-        <div key={title}>{title}</div>
+      {workList.map(({ title, description, link, image }, index) => (
+        <a key={index} className="work-item" href={link} target="_blank" rel="noopener noreferrer">
+          <div className="work-item-image">
+            <img src={image} alt={title} />
+          </div>
+          <div className="work-item-content">
+            <div className="work-item-title">{title}</div>
+            <div className="work-item-description">{description}</div>
+          </div>
+        </a>
       ))}
     </div>
   );
